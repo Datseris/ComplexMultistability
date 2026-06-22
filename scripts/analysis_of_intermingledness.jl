31739a74a7daaf296f57dd94507ee8d6120e14ed
Datseris <datseris.george@gmail.com>
2026-06-22 09:55:54 +0100

    add compat to attractors + finish intermingled

--- scripts/analysis_of_intermingledness.jl
+++ scripts/analysis_of_intermingledness.jl
@@ -1,19 +1,18 @@
-# This script compares analyses intermingledness
-# the first component of the script compares it with basin entropy
+# This script analyses intermingledness, boundary intermingledness, and basin entropy
+# It uses the definitions from Attractors.jl
 using DrWatson
 @quickactivate "ComplexMultistability"
 using PredefinedDynamicalSystems, Attractors
 include(srcdir("papertheme.jl"))
 # we don't include the intermingledness src file, the function in Attractors.jl is better
-
-ds = PredefinedDynamicalSystems.magnetic_pendulum(d=0.2, α=0.2, ω=0.8, N=3)
+N = 100
+ds = PredefinedDynamicalSystems.magnetic_pendulum(d=0.2, α=0.1, ω=0.8, N=3)
 psys = ProjectedDynamicalSystem(ds, [1, 2], [0.0, 0.0])
-xg = yg = range(-4, 4; length = 200)
+xg = yg = range(-4, 4; length = N)
 grid = (xg, yg)
 mapper = AttractorsViaRecurrences(psys, grid; Δt = 1)
 basins, attractors = basins_of_attraction(mapper, grid; show_progress = false)
 
-set_parameter!(psys, :γs, [1.0, 1.0, 1.0])
 set_parameter!(psys, :α, 1.0)
 reset_mapper!(mapper)
 # mapper = AttractorsViaRecurrences(psys, (xg, yg); Δt = 1)
@@ -27,17 +26,34 @@ basins3, attractors3 = basins_of_attraction(mapper, grid; show_progress = false)
 
 points = ics_from_grid(grid)
 
+# also prepare veros basin data
+input_file_name = "Veros_Data_F8_1.nc"
+include("veros_shared_code.jl")
+# also need initial conditions
+allu0s = zeros(length(ics), length(diagnostics))
+for i in eachindex(ics)
+    slice = X[InitCond(i)]
+    # To get actual initial condition is not trivial due to different evolution time
+    # so we find the first non-missing entry
+    x = gnv(slice[Diagnostic(1)])
+    j = findfirst(!ismissing, x)
+    allu0s[i, :] .= gnv(slice[Time(j)])
+end
+
+veros_basins = best_labels
+veros_points = StateSpaceSet(allu0s[:, best_choice[[1, 2]]])
+veros_points = rescale_to_01(veros_points)
+
 # %%
 
 # finally, make a basin with a smaller basin inside
-basins4 = ones(Int, 200, 200)
-L = 20
-for i in 100-L:100+L
-    for j in 100-L:100+L
+basins4 = ones(Int, N, N)
+L = N÷10
+for i in (N÷2)-L:(N÷2)+L
+    for j in (N÷2)-L:(N÷2)+L
         basins4[i, j] = 2
     end
 end
-basins5 = rand(1:2, 200, 200)
 
 fig = Figure(size = (figwidth, 2figheight))
 
@@ -50,10 +66,10 @@ for (i, b) in enumerate((basins, basins2, basins3))
     end
 
     heatmap!(ax, xg, yg, b; colormap = to_color.([COLORS[1], COLORS[2], COLORS[3]]))
+    be = Attractors.basin_entropy(b, L)[1]
     inter = Attractors.intermingledness(points, vec(b)) # points and labels must have same layout
-    v = mean(values(inter))
-    be = Attractors.basin_entropy(b, 5)[1]
-    ax.title = "intermingledness = $(round(v; digits = 1))\nbasin entropy = $(round(be; digits = 1))"
+    inter2 = Attractors.boundary_intermingledness(points, vec(b)) # points and labels must have same layout
+    ax.title = "intermingledness = $(round(mean(values(inter)); digits = 2))\nb-intermingledness = $(round(mean(values(inter2)); digits = 2))\nbasin entropy = $(round(be; digits = 2))"
     hidedecorations!(ax)
 
     if i == 1
@@ -65,26 +81,31 @@ for (i, b) in enumerate((basins, basins2, basins3))
 
 end
 
-inter4 = Attractors.intermingledness(points, vec(basins4)) # points and labels must have same layout
 ax4, = heatmap(fig[2,1], basins4; colormap = to_color.([COLORS[1], COLORS[2]]))
-ax4.title = "intermingledness (purple) = $(round(inter4[1]; digits = 1))\nintermingledness (black) = $(round(inter4[2]; digits = 1))"
+inter4 = Attractors.intermingledness(points, vec(basins4)) # points and labels must have same layout
+inter4new = Attractors.boundary_intermingledness(points, vec(basins4)) # points and labels must have same layout
+ax4.title = "intermingledness (purple) = $(round(maximum(values(inter4)); digits = 2))\n b-intermingledness (purple) = $(round(maximum(values(inter4new)); digits = 2))"
 hidedecorations!(ax4)
 
-inter5 = Attractors.intermingledness(points, vec(basins5))
-ax5, = heatmap(fig[2,2], basins5; colormap = to_color.([COLORS[1], COLORS[2]]))
-ax5.title = "intermingledness = $(round(mean(values(inter5)); digits = 1))"
-hidedecorations!(ax5)
+basins6 = ones(Int, N, N)
+for i in 1:(N÷5):N
+    basins6[i:(i + N÷10), :] .= 2
+end
+ax6, = heatmap(fig[2,2], basins6; colormap = to_color.([COLORS[1], COLORS[2]]))
+inter6 = Attractors.intermingledness(points, vec(basins6))
+inter6new = Attractors.boundary_intermingledness(points, vec(basins6))
+ax6.title = "intermingledness = $(round(mean(values(inter6)); digits = 2))\nb-intermingledness = $(round(mean(values(inter6new)); digits = 2))"
+hidedecorations!(ax6)
 
-# finally, completely separated basins without touching
-randompoint() = [rand([-1, 1]) + 0.1randn(), 0.1randn()]
-points2 = [randompoint() for _ in 1:1000]
-labels6 = [x[1] < 0 ? 1 : 2 for x in points2]
-inter6 = Attractors.intermingledness(points2, labels6)
-ax6, = scatter(fig[2,3], StateSpaceSet(points2); color = labels6, colormap = [COLORS[1], COLORS[2]])
-ax6.title = "intermingledness = $(round(mean(values(inter6)); digits = 1))"
+# do veros basins here  something better here:
+ax5, = scatter(fig[2,3], veros_points; color = [COLORS[l] for l in best_labels])
+inter5 = Attractors.intermingledness(veros_points, veros_basins)
+inter5new = Attractors.boundary_intermingledness(veros_points, veros_basins)
+ax5.title = "intermingledness (yellow) = $(round(maximum(values(inter5)); digits = 2))\nb-intermingledness (yellow) = $(round(maximum(values(inter5new)); digits = 2))"
+hidedecorations!(ax5)
 
 fig
 
 # %%
-wsave(papersdir("figures", "compare_with_be"), fig)
-negate_remove_bg(papersdir("figures", "compare_with_be.png"))
+wsave(papersdir("figures", "intermingledness"), fig)
+negate_remove_bg(papersdir("figures", "intermingledness.png"))
